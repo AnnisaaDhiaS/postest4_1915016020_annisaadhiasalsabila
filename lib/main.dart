b39a222d556ae44757717c5e857577966ab7ed34
Annisaa' Dhia S <annisaadstia@gmail.com>
2022-04-26 21:32:08 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:postest4_1915016020_annisaadhiasalsabila/main_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Posttest 4 Annisaa Dhia Salsabla 1915016020",
+      home: main_page(),
+    );
+  }
+}
